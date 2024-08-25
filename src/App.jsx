25beb95a4af88f926083ed193db17058acbf5876
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2024-08-25 11:10:44 +0530

    study about condition in useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import UseState from "./Components/UseState";
+import UseStateD_2 from "./Components/UseStateD_2";
+import UseStateD_3 from "./Components/UseStateD_3";
 
 const App = () => {
 
   return (
     <div>
-      <UseState />
+      {/* <UseState /> */}
+      {/* <UseStateD_2 /> */}
+      <UseStateD_3 />
     </div>
   );
 };
