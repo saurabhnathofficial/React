12979436ff0e4d7636ab8293c6b1b9a2c796087d
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2024-09-08 22:03:34 +0530

    button add

--- src/Components/LudoBoard.jsx
+++ src/Components/LudoBoard.jsx
@@ -4,17 +4,29 @@ function LudoBoard() {
 
   let [moves, setMoves]= useState({blue: 0, red: 0, green: 0, yellow: 0 });
 
-  let updateBlue = ()=>{    
+  let updateBlue1 = ()=>{    
     setMoves({...moves, blue: moves.blue += 1});
   }
-  let updateYellow = ()=>{    
+  let updateBlue2 = ()=>{    
+    setMoves({...moves, blue: moves.blue -= 1});
+  }
+  let updateYellow1 = ()=>{    
     setMoves({...moves, yellow: moves.yellow += 1});
   }
-  let updateRed = ()=>{    
+  let updateYellow2 = ()=>{    
+    setMoves({...moves, yellow: moves.yellow -= 1});
+  }
+  let updateRed1 = ()=>{    
     setMoves({...moves, red: moves.red += 1});
   }
-  let updateGreen = ()=>{    
+  let updateRed2 = ()=>{    
+    setMoves({...moves, red: moves.red -= 1});
+  }
+  let updateGreen1 = ()=>{    
     setMoves({...moves, green: moves.green += 1});
+  }
+  let updateGreen2 = ()=>{    
+    setMoves({...moves, green: moves.green -= 1});
   }
   return (
     <div>
@@ -22,16 +34,28 @@ function LudoBoard() {
       <div className="bg-zinc-200 flex flex-col gap-2 items-start pl-4 rounded py-4">
 
         <p className='font-bold text-xl'>Blue Moves = {moves.blue}</p>
-        <button className='px-8 py-2 bg-blue-500 rounded-full text-white font-semibold' onClick={updateBlue}>+1</button>
+        <div className="flex gap-2">
+        <button className='px-8 py-2 bg-blue-500 rounded-full text-white font-semibold' onClick={updateBlue1}>+1</button>
+        <button className='px-8 py-2 bg-blue-300 rounded-full text-white font-semibold' onClick={updateBlue2}>-1</button>
+        </div>
 
         <p className='font-bold text-xl'>Yellow Moves = {moves.yellow}</p>
-        <button className='px-8 py-2 bg-yellow-500 rounded-full text-white font-semibold' onClick={updateYellow}>+1</button>
+        <div className="flex gap-2">
+        <button className='px-8 py-2 bg-yellow-500 rounded-full text-white font-semibold' onClick={updateYellow1}>+1</button>
+        <button className='px-8 py-2 bg-yellow-300 rounded-full text-white font-semibold' onClick={updateYellow2}>-1</button>
+        </div>
 
         <p className='font-bold text-xl'>Green Moves = {moves.green}</p>
-        <button className='px-8 py-2 bg-green-500 rounded-full text-white font-semibold ' onClick={updateGreen}>+1</button>
+        <div className="flex gap-2">
+        <button className='px-8 py-2 bg-green-500 rounded-full text-white font-semibold ' onClick={updateGreen1}>+1</button>
+        <button className='px-8 py-2 bg-green-300 rounded-full text-white font-semibold ' onClick={updateGreen2}>-1</button>
+        </div>
 
         <p className='font-bold text-xl'>Red Moves = {moves.red}</p>
-        <button className='px-8 py-2 bg-red-500 rounded-full text-white font-semibold ' onClick={updateRed}>+1</button>
+        <div className="flex gap-2">
+        <button className='px-8 py-2 bg-red-500 rounded-full text-white font-semibold ' onClick={updateRed1}>+1</button>
+        <button className='px-8 py-2 bg-red-300 rounded-full text-white font-semibold ' onClick={updateRed2}>-1</button>
+        </div>
 
       </div>
     </div>
