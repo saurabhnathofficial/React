2a52447e0659a3a6d09633b0689a89bd277426e2
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2024-09-11 22:35:49 +0530

    mark feature adde

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   return (
     <>
-    <div className="w-full h-screen bg-zinc-500 flex justify-center items-center text-3xl ">
+    <div className="w-full h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center text-3xl " id="main">
     <Todo />
     </div>
     </>
--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -1,44 +1,73 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaS } from 'react-icons/fa6';
 import { v4 as uuidv4 } from 'uuid';
 
 function Todo() {
 
-    let [todos, setTodos] = useState([{task: "Sample Task", id: uuidv4()}]);
+    let [todos, setTodos] = useState([{ task: "Sample Task", id: uuidv4(), isDone: false,  }]);
     let [newTodo, setNewTodo] = useState("");
+    // let [isDone, setIsDone] = useState[false];
 
     let addNewTask = () => {
-        setTodos([...todos, newTodo])
+        setTodos((prevTodos) => {
+            return [...prevTodos, { task: newTodo, id: uuidv4() }];
+        })
+        setNewTodo("");
     }
-    let deleteTodo = (index) => {
-        console.log(index)
+    let deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id != id));
+    };
+
+    let markAsDone = (id) => {
+        setTodos((prevTodos) =>
+            prevTodos.map((todo) => {
+                if (todo.id == id) {
+                    return {
+                        ...todo,
+                        isDone: true,
+                    }
+                } else {
+                    return {
+                        ...todo,
+                    }
+                }
+            })
+        )
     }
+ 
+
     let updateTodoValue = (event) => {
         setNewTodo(event.target.value);
         // console.log(event.target.value)
     }
     return (
-        <div className='h-3/4 w-1/2 bg-zinc-100 rounded-sm p-4 flex flex-col gap-3'>
+        <div className='h-min w-1/2 rounded-sm p-4 flex flex-col gap-3 bg-gradient-to-r md:bg-gradient-to-l'>
             <div className="text-4xl font-bold justify-center flex border-2">
                 <p>ToDo</p>
 
             </div>
             <div className="flex justify-between gap-4 ">
-                <input className='text-sm p-2 bg-zinc-500 rounded w-full' type="text" placeholder='Enter a Task' value={newTodo} onChange={updateTodoValue} />
+                <input className='text-sm p-2 bg-gradient-to-r from-slate-300 to-slate-500  rounded w-full' type="text" placeholder='Enter a Task' value={newTodo} onChange={updateTodoValue} />
                 <button onClick={addNewTask} className='w-40 font-semibold px-4 py-2 bg-sky-500 rounded text-sm'>Add Task</button>
             </div>
 
-            <div className="w-full h-full bg-zinc-300 text-xl font-semibold capitalize p-2 ">
-                <ol className=''>
-                    {todos.map((todo, index) => (
-                        <li key={index} className='flex gap-3 mb-2 justify-between border-2 border-orange-200 px-4 py-1'>
-                            <span>{todo}</span>
-                            <button onClick={deleteTodo(index)} className='font-semibold px-2 py-1 bg-red-300 rounded text-sm'>Delete</button>
+            <div className="w-full h-full bg-gradient-to-r from-violet-200 to-pink-200 text-xl font-semibold capitalize p-2 ">
+                <ul>
+                    {todos.map((todo,index) => (
+                        <li key={todo.id} className='flex gap-3 mb-2 bg-gradient-to-r from-blue-200 to-cyan-200 justify-between border-2 border-orange-200 px-4 py-1 text-1xl'>
+                            <span style={todo.isDone ? { textDecorationLine: "line-through" } : {}}>{index} : {todo.task}</span>
+                            <div className="flex gap-3">
+                                <button onClick={() => markAsDone(todo.id)} className={`font-semibold px-2 py-1 ${todo.isDone ? "bg-green-500" : "bg-red-200"} rounded text-sm`}>Mark as Done</button>
+                                <button onClick={() => deleteTodo(todo.id)} className='font-semibold px-2 py-1 bg-red-300 rounded text-sm'>Delete</button>
+                            </div>
                         </li>
 
                     ))}
-                </ol>
+                </ul>
+
             </div>
+
         </div>
     )
 }
