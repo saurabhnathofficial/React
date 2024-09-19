9aaaed469004bfd720b4c51695951f545b2d8272
saurabhnathofficial <saurabhnathofficial01@gmail.com>
2024-09-19 21:11:29 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Api from "./Components/ApiCalling/Api";
+import Whether from "./Components/whether/Whether";
 
 const App = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center text-xl " id="main">
-        <Api  />
+      <div className="w-full h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center text-2xl text-white font-semibold">
+        <Whether  />
       </div>
     </>
   );
